d01f24518f5f90fb7bff4b341d54e07beae8a512
Levi Lucena <sr_lucen@yahoo.com.br>
2023-07-17 10:14:38 -0300

    Update disco-rigido.php

--- Inventário/disco-rigido.php
+++ Inventário/disco-rigido.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+<!-- ====================================================================== -->
+<!--  Desenvolvido por Levi Lucena - linkedin.com/in/levilucena -->
+<!-- ====================================================================== -->
 <html>
 <head>
 <link rel="stylesheet" href="style.css">
